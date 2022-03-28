30a74b2687a13ec76470ef14d822ed42fddaf490
Martin Dousa <martindousa186@gmail.com>
2022-03-29 00:01:55 +0200

    ...

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,11 @@ project "yaml-cpp"
   targetdir("bin/" .. outputdir .. "/%{prj.name}")
   objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 
+  defines
+  {
+    "YAML_CPP_STATIC_DEFINE"
+  }
+
   files
   {
     "src/**.h",
